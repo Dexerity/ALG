35a3170d468e1022d5fd3142d6414bba1be7b8ed
Martin Ozdinec <ozd.martin@gmail.com>
2024-02-28 11:10:56 +0100

    Lab2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 
+bool checkPParity(string text);
+
 struct Stack
 {
-    int capacity = 3;
-    int data[3];
+    int capacity = 10;
+    int data[10];
     int index = 0;
 
     void push(int value)
@@ -14,11 +16,87 @@ struct Stack
         else
             cout << "Stack is full!" << endl;
     }
-    int get();
+
+    int pop()
+    {
+        if(index > 0)
+            return data[--index];
+        else
+        {
+            cout << "Stack is empty!" << endl;
+            return INT32_MIN;
+        }
+    }
+
+    int top()
+    {
+        if(index > 0)
+            return data[index - 1];
+        else
+        {
+            cout << "Stack is empty!" << endl;
+            return INT32_MIN;
+        }
+    }
+
+    bool isEmpty()
+    {
+        return !index;
+    }
 };
 
 int main() {
-    Stack s;
+    string text = "([(text[])])({})(<>)";
+    
+    if (checkPParity(text))
+        printf("Correct text parity\n");
+    else
+        printf("Incorrect text parity\n");
+    
+
 
     return 0;
+}
+
+bool checkPParity(string text)
+{
+    Stack s;
+    for (char c : text) 
+    {
+        if (c == '(' || c == '[' || c == '{' || c == '<')
+            s.push(c);
+        else if (c == ')') 
+        {
+            if (s.top() != '(') {
+                return false;
+            }
+            s.pop();
+        }
+        else if (c == ']') 
+        {
+            if (s.top() != '[') {
+                return false;
+            }
+            s.pop();
+        }
+        else if (c == '}') 
+        {
+            if (s.top() != '{') {
+                return false;
+            }
+            s.pop();
+        }
+        else if (c == '>') 
+        {
+            if (s.top() != '<') {
+                return false;
+            }
+            s.pop();
+        }
+        else 
+        {
+            continue;
+        }
+    }
+    return true;
 }
